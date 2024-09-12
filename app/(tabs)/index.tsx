32d70d2464b786629417825c8151b9ac4e902da6
GustavoZeglan <luizgustavozeglan1@gmail.com>
2024-09-12 20:58:14 -0300

    implementing flatList

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,26 @@
-import { StyleSheet, SafeAreaView } from 'react-native';
+import Recipe from '@/models/Recipe';
+import axios from 'axios';
+import { useCallback, useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
+  const [recipes, setRecipes] = useState<Recipe[]>(); 
+
+  const getRecipes = useCallback(async () => {
+    const res = await axios.get("https://api.spoonacular.com/recipes/complexSearch?apiKey=dea213cec7414c7f86df23e4b995a954", {});
+    
+    setRecipes(res.data.results);
+  },[]);
+  
+  useEffect(() => {
+    getRecipes();
+  },[]);
+
   return (
    <SafeAreaView>
-
+      <Text>Bah...</Text>
+      <FlatList data={recipes} renderItem={(item) => <Text>{item.item.title}</Text>}/>
    </SafeAreaView>
   );
 }
