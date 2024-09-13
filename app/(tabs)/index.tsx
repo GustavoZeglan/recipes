36705ace9c4dddc355ce0841f97deac19c0c6a18
GustavoZeglan <luizgustavozeglan1@gmail.com>
2024-09-12 21:06:25 -0300

    implementing input for search

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,41 @@
 import Recipe from '@/models/Recipe';
 import axios from 'axios';
 import { useCallback, useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text } from 'react-native';
+import { FlatList, StyleSheet, Text, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
-  const [recipes, setRecipes] = useState<Recipe[]>(); 
+  const [recipes, setRecipes] = useState<Recipe[]>();
+  const [name, setName] = useState<string>("");
 
-  const getRecipes = useCallback(async () => {
-    const res = await axios.get("https://api.spoonacular.com/recipes/complexSearch?apiKey=dea213cec7414c7f86df23e4b995a954", {});
+  const getRecipes = useCallback(async (query: string) => {
+    const res = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=dea213cec7414c7f86df23e4b995a954`, {});
     
     setRecipes(res.data.results);
   },[]);
   
   useEffect(() => {
-    getRecipes();
-  },[]);
+    getRecipes(name);
+  },[name]);
 
   return (
-   <SafeAreaView>
-      <Text>Bah...</Text>
+   <SafeAreaView style={styles.Container}>
+      <TextInput style={styles.inputName}
+                           placeholder={"Digite o nome da receita"}
+                           placeholderTextColor={"white"} 
+                           onChangeText={setName}></TextInput>
       <FlatList data={recipes} renderItem={(item) => <Text>{item.item.title}</Text>}/>
    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  Container: {
+    flex: 1,
+    flexDirection: 'column',
     alignItems: 'center',
     gap: 8,
+    paddingVertical: 24,
   },
   stepContainer: {
     gap: 8,
@@ -42,4 +48,14 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  inputName: {
+    backgroundColor: "#7f8ea7",
+    width: "90%",
+    paddingVertical: 16,
+    display: "flex",
+    alignItems: "center",
+    borderRadius: 8,
+    fontSize: 18,
+    paddingLeft: 16,
+},
 });
